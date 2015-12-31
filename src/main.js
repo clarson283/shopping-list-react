e0d4a525dc55891e298f9bf7a44e5e045c94b403
Callie <clarson283@gmail.com>
2015-12-31 13:47:46 -0800

    breakdown

---
+++ src/main.js
@@ -0,0 +1,7 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var classNames = require('classnames');
+var ListApp = require('./list-app.js'); //components get first letter caps - no camelCase
+
+
+ReactDOM.render(<ListApp />, document.getElementById('main'));
